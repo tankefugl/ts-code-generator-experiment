5f4585e0abbd3b1f79653af7b392cda6df3975de
Petter Rønningen <tankefugl@gmail.com>
2022-04-23 08:05:28 +0000

    Linting on save, but some issues

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,67 @@
-function squint(target: string): string {
-  return `squint at ${target}!`
+import ts from 'typescript';
+
+function makeFactorialFunction() {
+  const functionName = ts.factory.createIdentifier('factorial');
+  const paramName = ts.factory.createIdentifier('n');
+  const parameter = ts.factory.createParameterDeclaration(
+    /*decorators*/ undefined,
+    /*modifiers*/ undefined,
+    /*dotDotDotToken*/ undefined,
+    paramName,
+  );
+
+  const condition = ts.factory.createBinaryExpression(
+    paramName,
+    ts.SyntaxKind.LessThanEqualsToken,
+    ts.factory.createNumericLiteral(1),
+  );
+  const ifBody = ts.factory.createBlock(
+    [ts.factory.createReturnStatement(ts.factory.createNumericLiteral(1))],
+    /*multiline*/ true,
+  );
+
+  const decrementedArg = ts.factory.createBinaryExpression(
+    paramName,
+    ts.SyntaxKind.MinusToken,
+    ts.factory.createNumericLiteral(1),
+  );
+  const recurse = ts.factory.createBinaryExpression(
+    paramName,
+    ts.SyntaxKind.AsteriskToken,
+    ts.factory.createCallExpression(functionName, /*typeArgs*/ undefined, [
+      decrementedArg,
+    ]),
+  );
+  const statements = [
+    ts.factory.createIfStatement(condition, ifBody),
+    ts.factory.createReturnStatement(recurse),
+  ];
+
+  return ts.factory.createFunctionDeclaration(
+    /*decorators*/ undefined,
+    /*modifiers*/ [ts.factory.createToken(ts.SyntaxKind.ExportKeyword)],
+    /*asteriskToken*/ undefined,
+    functionName,
+    /*typeParameters*/ undefined,
+    [parameter],
+    /*returnType*/ ts.factory.createKeywordTypeNode(
+      ts.SyntaxKind.NumberKeyword,
+    ),
+    ts.factory.createBlock(statements, /*multiline*/ true),
+  );
 }
 
-const result = squint("cat");
+const rootNode = makeFactorialFunction();
 
+const file = ts.createSourceFile(
+  'out.ts',
+  '',
+  ts.ScriptTarget.ESNext,
+  false,
+  ts.ScriptKind.TS,
+);
+const printer = ts.createPrinter({
+  newLine: ts.NewLineKind.LineFeed,
+});
+const result = printer.printNode(ts.EmitHint.Unspecified, rootNode, file);
 console.log(result);
